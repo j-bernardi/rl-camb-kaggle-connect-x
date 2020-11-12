030769289c1d98f74bd98f1714c17a605d57b7d2
James Bernardi <bernardi.james.h@gmail.com>
2020-11-12 23:20:12 +0000

    adding custom reward functionality to env wrapper

--- utils/env_wrapper.py
+++ utils/env_wrapper.py
@@ -9,11 +9,22 @@ class ConnectX(gym.Env):
     https://www.kaggle.com/phunghieu/connectx-with-deep-q-learning
     """
 
-    def __init__(self, score_target=80, out_of=100, debug=False):
+    def __init__(
+        self, score_target=80, out_of=100, debug=False,
+        step_r=0.5, lose_r=0., draw_r=0.5, win_r=1.):
         # Set the env
         # score target = won of last out_of
         self.env = ke.make("connectx", debug=debug)
         self.configuration = self.env.configuration
+
+        # (lose, draw, win) tuple
+        self.standard_rewards = self.env.specification.reward.enum
+
+        self.step_r = step_r
+        self.lose_r = lose_r
+        self.draw_r = draw_r
+        self.win_r = win_r
+
         self.score_target = score_target
         self.out_of = out_of
         
@@ -39,24 +50,45 @@ class ConnectX(gym.Env):
             score_dict: a collection of the last self.out_of
                 games' outcomes
         """
-        vals = np.array(self.env.specification.reward.enum).astype(float)
-
         if len(game_scores) > self.out_of:
 
             score_vals, counts = np.unique(
                 game_scores[-self.out_of:], return_counts=True
             )
             # Ensure game scores are consistent
-            # If draws are present, they are all floats
             score_vals = np.round(score_vals.astype(float), 1)
-            assert all(v in vals for v in score_vals)
+            # Check we haven't messed up what's going in the score dict
+            assert all(v in self.standard_rewards for v in score_vals),\
+                f"{score_vals} != {self.standard_rewards}"
             score_dict = dict(zip(score_vals, counts))
-            solved = score_dict[1.0] > self.env_wrapper.score_target
+            solved = score_dict.get(
+                self.standard_rewards[2], 0) > self.score_target
         else:
             score_dict = None
             solved = False
 
         return solved, score_dict
+
+    def custom_reward(self, reward, done):
+        """
+        Experiment ideas:
+          - Go for the win! Penalise loss as much as draw
+          - Take your time. Ignore negative steps
+        """
+
+        if reward == self.standard_rewards[2]:
+            return self.win_r
+        elif reward == self.standard_rewards[0]:
+            return self.lose_r
+        elif reward == self.standard_rewards[1] and done:
+            return self.draw_r
+        elif reward == self.standard_rewards[1] and not done:
+            return self.step_r
+        else:
+            raise RuntimeError(
+                f"Unexpected reward {reward} not in {self.standard_rewards}, "
+                f"done: {done}"
+        )
 
     def switch_trainer(self, switch_to):
         """
